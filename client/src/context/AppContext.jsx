b6a34ce05acdd501549e50ea85368db98132f582
EmilMaria <emilmaria2570@gmail.com>
2025-04-24 07:19:54 +0530

    Finish frontend development

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from 'humanize-duration'
 
 export const AppContext = createContext()
 
@@ -11,6 +12,7 @@ export const AppContextProvider = (props)=>{
 
     const [allCourses, setAllCourses] = useState([])
     const [isEducator, setIsEducator] = useState(true)
+    const [enrolledCourses, setEnrolledCourses] = useState([])
 
     // Fetch All Courses
     const fetchAllCourses = async ()=>{
@@ -29,12 +31,48 @@ export const AppContextProvider = (props)=>{
         return totalRating / course.courseRatings.length
     }
 
+    // Function to Calculate Course Chapter Time
+    const calculateChapterTime = (chapter)=>{
+        let time = 0
+        chapter.chapterContent.map((lecture)=> time += lecture.lectureDuration)
+        return humanizeDuration(time * 60 * 1000, {units: ["h","m"]})
+    }
+
+    // Function to Calculate Course Duration
+    const calculateCourseDuration = (course)=>{
+        let time = 0
+
+        course.courseContent.map((chapter)=> chapter.chapterContent.map((lecture) => time += lecture.lectureDuration
+    ))
+    return humanizeDuration(time * 60 * 1000, {units: ["h","m"]})
+
+    }
+
+    // Function calculate to No of Lectures in the course
+    const calculateNoOfLectures = (course)=>{
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter =>{
+            if(Array.isArray(chapter.chapterContent)){
+                totalLectures += chapter.chapterContent.length;
+            }
+        });
+        return totalLectures;
+    }
+
+    // Fetch User Enrolled Courses
+    const fetchUserEnrolledCourses = async ()=>{
+        setEnrolledCourses(dummyCourses)
+    }
+
+
+
 useEffect(()=>{
     fetchAllCourses()
+    fetchUserEnrolledCourses()
 },[])
 
 const value ={
-    currency, allCourses, navigate ,calculateRating,isEducator,setIsEducator
+    currency, allCourses, navigate ,calculateRating,isEducator,setIsEducator ,calculateNoOfLectures ,calculateCourseDuration ,calculateChapterTime ,enrolledCourses ,fetchUserEnrolledCourses
 
 }
 
